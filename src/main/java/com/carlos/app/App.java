09e75a511237e5c17d22b331c9e6e0b47e00b347
Carlos <carlosbpac@gmail.com>
2020-07-23 17:44:38 -0600

    creacion del archivo logica

--- src/main/java/com/carlos/app/App.java
+++ src/main/java/com/carlos/app/App.java
@@ -1,13 +1,9 @@
 package com.carlos.app;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Clase pirncipal del programa
     }
 }
---
+++ src/main/java/com/carlos/app/Logica.java
@@ -0,0 +1,4 @@
+package com.carlos.app;
+class Logica {
+    
+}
