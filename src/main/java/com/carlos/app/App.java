c9ab9b10c2afef62f366186d5e131b71dfcf290d
Carlos <carlosbpac@gmail.com>
2020-07-23 20:50:59 -0600

    Recocimiento de Secuencias

--- src/main/java/com/carlos/app/App.java
+++ src/main/java/com/carlos/app/App.java
@@ -4,8 +4,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Se ejecuto el programa");
         Logica analisis = new Logica();
-        
+        String cadena1 = "UFJHGFHJSDGFJGSDFJ";
+        String cadena2 = "YTWUFSJBLISEMUFDHU";
+        analisis.analisisDeDatos(cadena1, cadena2);
 
     }
 }
--- src/main/java/com/carlos/app/Logica.java
+++ src/main/java/com/carlos/app/Logica.java
@@ -1,8 +1,62 @@
 package com.carlos.app;
 
+
 public class Logica {
     //Constructor vacio del archivo logica
     public Logica(){
 
+    }
+    public void analisisDeDatos(String cadena1, String cadena2){
+        System.out.println(cadena1);
+        System.out.println(cadena2); 
+        int posicionDeExtracccion=0;
+        String primeraletradelacadena1;
+        int posicion = 0;
+        int caracter1 = 0;
+        int caracter2 = 0;
+        int cantidadDeConcidencias=0;
+        while(posicionDeExtracccion<cadena1.length()){
+            primeraletradelacadena1=String.valueOf(cadena1.charAt(posicionDeExtracccion)); 
+            
+            if(cadena2.indexOf(primeraletradelacadena1, posicion)!= -1)
+            {
+                
+                while(cadena2.indexOf(primeraletradelacadena1, posicion)>-1){
+                    caracter2 = cadena2.indexOf(primeraletradelacadena1, posicion);
+                    posicion= caracter2+1;
+                    if((posicionDeExtracccion+1) < cadena1.length()&&(caracter2+1)<cadena2.length()){
+                        int posicionDeComparacion1 = posicionDeExtracccion;
+                        int posicionDeComparacion2 = caracter2; 
+
+
+                        while(cadena1.charAt(posicionDeComparacion1+1)==cadena2.charAt(posicionDeComparacion2+1)){
+                            cantidadDeConcidencias++;
+                            if((posicionDeComparacion1+1)<cadena1.length())
+                            {
+                                posicionDeComparacion1++;
+                            }
+                            if((posicionDeComparacion2+1)<cadena2.length())
+                            {
+                                posicionDeComparacion2++;
+                            }
+                        }
+                        if(cantidadDeConcidencias>0){
+                            System.out.println("La cantidad de coincidencias es: "+ cantidadDeConcidencias);
+                        }
+                        cantidadDeConcidencias=0;
+                        /*
+                        if(cadena1.charAt(posicionDeComparacion1+1)!=cadena2.charAt(posicionDeComparacion2+1)){
+                            System.out.println(primeraletradelacadena1);
+                            System.out.println(caracter2);
+                            System.out.println("Candidato de analisis");
+                        }*/
+                    }
+                }
+            }
+            posicion=0;
+            posicionDeExtracccion++;
+        }
+   
+        
     }
 }
